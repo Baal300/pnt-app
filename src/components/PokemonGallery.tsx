2ccf4c53e4b909a8111096ba1a26d642a852458b
Dat Do <duytiendatdo@yahoo.de>
2025-10-13 05:23:40 +0200

    Change gallery sizing

--- src/components/PokemonGallery.tsx
+++ src/components/PokemonGallery.tsx
@@ -7,14 +7,18 @@ type PokemonGalleryProps = {
 };
 
 export const PokemonGallery = ({ pokemonList }: PokemonGalleryProps) => (
-  <div className="grid grid-cols-5 gap-4">
+  <div className="grid grid-cols-4 gap-3 sm:grid-cols-5">
     {pokemonList.map((poke) => (
-      <div key={poke.number} className="flex flex-col items-center">
-        <img src={poke.image} alt={poke.name.english} className="h-32 w-32" />
-        <span className="mt-2 font-bold">#{poke.number}</span>
-        <span className="mt-2 font-bold">{poke.name.english} (EN)</span>
-        <span className="mt-2 font-bold">{poke.name.german} (DE)</span>
-        <span className="mt-2 font-bold">{poke.name.japanese} (JP)</span>
+      <div key={poke.number} className="mb-2 flex flex-col items-center">
+        <img
+          src={poke.image}
+          alt={poke.name.english}
+          className="h-32 w-32 border-1 bg-amber-100"
+        />
+        <span className="mt-1 font-bold">#{poke.number}</span>
+        <span className="mt-1 font-bold">{poke.name.english} (EN)</span>
+        <span className="mt-1 font-bold">{poke.name.german} (DE)</span>
+        <span className="mt-1 font-bold">{poke.name.japanese} (JP)</span>
       </div>
     ))}
   </div>
