33c4bb7da2110d9173485c56c9ef69a7801bb3e8
Dat Do <duytiendatdo@yahoo.de>
2025-10-21 19:43:15 +0200

    Add error handling to fetch functions in api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,42 +1,67 @@
 import type { PokemonDataResponse } from "../types";
 
 export async function fetchPokemonNames(): Promise<string[]> {
-  const res = await fetch(
-    "https://pokeapi.co/api/v2/pokemon-species?limit=10000",
-  );
-  const data = await res.json();
-  if (!data.results) throw new Error("Failed to fetch Pokémon names");
-  return data.results.map((poke: { name: string }) => poke.name);
+  try {
+    const res = await fetch(
+      "https://pokeapi.co/api/v2/pokemon-species?limit=10000",
+    );
+
+    const data = await res.json();
+    if (!data.results) throw new Error("Failed to fetch Pokémon names");
+    return data.results.map((poke: { name: string }) => poke.name);
+  } catch (error) {
+    console.error("Error fetching Pokémon names:", error);
+    return [];
+  }
 }
 
 export async function fetchPokemonByRegion(start: number, end: number) {
-  const res = await fetch(
-    `https://pokeapi.co/api/v2/pokemon?limit=${end - start + 1}&offset=${start - 1}`,
-  );
-
-  if (!res.ok) {
-    throw new Error(
-      `Error fetching Pokémon by region: ${res.status} ${res.statusText}`,
+  try {
+    const res = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${end - start + 1}&offset=${start - 1}`,
     );
-  }
 
-  const data = await res.json();
-  return data.results;
+    if (!res.ok) {
+      throw new Error(
+        `Error fetching Pokémon by region: ${res.status} ${res.statusText}`,
+      );
+    }
+    const data = await res.json();
+    return data.results;
+  } catch (error) {
+    console.error("Error fetching Pokémon by region:", error);
+    return [];
+  }
 }
 
 export async function fetchPokemonDetails(pokeUrl: string) {
-  const res = await fetch(pokeUrl);
-  return await res.json();
+  try {
+    const res = await fetch(pokeUrl);
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching Pokémon details:", error);
+    return null;
+  }
 }
 
 export async function fetchSpeciesDetails(speciesUrl: string) {
-  const res = await fetch(speciesUrl);
-  return await res.json();
+  try {
+    const res = await fetch(speciesUrl);
+    return await res.json();
+  } catch (error) {
+    console.error("Error fetching species details:", error);
+    return null;
+  }
 }
 
 export const fetchMusic = async (API_URL: string) => {
-  const res = await fetch(`${API_URL}/api/music/1`);
-  return createAudioObjectURL(res);
+  try {
+    const res = await fetch(`${API_URL}/api/music/1`);
+    return createAudioObjectURL(res);
+  } catch (error) {
+    console.error("Error fetching music:", error);
+    return null;
+  }
 };
 
 const createAudioObjectURL = async (response: Response) => {
