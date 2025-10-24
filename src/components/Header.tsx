60f38f3b06554e246b37eee3268cc1fca2a1c59f
Dat Do <duytiendatdo@yahoo.de>
2025-10-24 20:27:42 +0200

    Make Navbar/Header sticky

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { ToggleDarkModeButton } from "./ToggleDarkModeButton";
 
 export const Header = () => {
   return (
-    <header className="navbar bg-primary drop-shadow-xl">
+    <header className="navbar bg-primary sticky top-0 z-10 drop-shadow-xl">
       <h1 className="text-primary-content flex-1 justify-center text-lg font-bold sm:text-3xl">
         Pokémon Name Translator
       </h1>
