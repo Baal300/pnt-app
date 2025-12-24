ac4290236bee6bc702a75133137e0e4ab0e62d76
Dat Do <duytiendatdo@yahoo.de>
2025-12-24 03:44:11 +0100

    Remove 3D hover effect for functionality purposes

--- src/components/Gallery/PokemonCard.tsx
+++ src/components/Gallery/PokemonCard.tsx
@@ -9,49 +9,39 @@ type PokemonCardProps = {
 
 export const PokemonCard = ({ pokemonInfo }: PokemonCardProps) => {
     return (
-        <div className="hover-3d justify-center">
-            <div
-                key={pokemonInfo.number}
-                className="card border-accent dark:from-start-gradient-dark dark:to-end-gradient-dark from-start-gradient to-end-gradient m-0.5 w-42 flex-col items-center border bg-linear-to-br p-2 text-center sm:m-1 sm:text-lg"
-            >
-                <div className="flex w-full flex-row items-center justify-center">
-                    <img
-                        src={pokemonInfo.image}
-                        alt={pokemonInfo.name.english}
-                        className="mx-2 mt-3 h-32 w-32 border-1 bg-amber-100 outline-1"
-                    />
-                </div>
-
-                <div className="card-body join join-vertical w-full gap-0 px-1.5 py-2 text-shadow-sm">
-                    <p className="join-item">#{pokemonInfo.number}</p>
-                    <p className="join-item flex flex-row items-center justify-between font-semibold">
-                        {pokemonInfo.name.english || "N/A"}{" "}
-                        <span>
-                            <UKFlag title="English" />
-                        </span>
-                    </p>
-                    <p className="join-item flex flex-row items-center justify-between font-semibold">
-                        {pokemonInfo.name.german || "N/A"}{" "}
-                        <span>
-                            <GermanyFlag title="German" />
-                        </span>
-                    </p>
-                    <p className="join-item flex flex-row items-center justify-between font-semibold">
-                        <span>{pokemonInfo.name.japanese || "N/A"}</span>
-                        <span>
-                            <JapanFlag title="Japanese" />
-                        </span>
-                    </p>
-                </div>
+        <div
+            key={pokemonInfo.number}
+            className="card border-accent dark:from-start-gradient-dark dark:to-end-gradient-dark from-start-gradient to-end-gradient m-0.5 w-42 flex-col items-center border bg-linear-to-br p-2 text-center sm:m-1 sm:text-lg"
+        >
+            <div className="flex w-full flex-row items-center justify-center">
+                <img
+                    src={pokemonInfo.image}
+                    alt={pokemonInfo.name.english}
+                    className="mx-2 mt-3 h-32 w-32 border-1 bg-amber-100 outline-1"
+                />
             </div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+
+            <div className="card-body join join-vertical w-full gap-0 px-1.5 py-2 text-shadow-sm">
+                <p className="join-item">#{pokemonInfo.number}</p>
+                <p className="join-item flex flex-row items-center justify-between font-semibold">
+                    {pokemonInfo.name.english || "N/A"}{" "}
+                    <span>
+                        <UKFlag title="English" />
+                    </span>
+                </p>
+                <p className="join-item flex flex-row items-center justify-between font-semibold">
+                    {pokemonInfo.name.german || "N/A"}{" "}
+                    <span>
+                        <GermanyFlag title="German" />
+                    </span>
+                </p>
+                <p className="join-item flex flex-row items-center justify-between font-semibold">
+                    <span>{pokemonInfo.name.japanese || "N/A"}</span>
+                    <span>
+                        <JapanFlag title="Japanese" />
+                    </span>
+                </p>
+            </div>
         </div>
     );
 };
